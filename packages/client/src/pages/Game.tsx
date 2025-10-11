b7da6830125b2ac7e8ec2e8868d591f0daac4de6
Mikhail Kravchenko <bjaka13was@mail.ru>
2025-10-11 16:27:02 +0300

    TASK-6: Добавлен движок игры

---
+++ packages/client/src/pages/Game.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react'
+import GameEngine from '../gameEngine/GameEngine'
+import { ESkillName } from '../gameEngine/types'
+import { SKILLS } from '../gameEngine/constants/skills'
+
+export const Game = () => {
+  useEffect(() => {
+    new GameEngine()
+  }, [])
+
+  return (
+    <>
+      <div id="container">
+        <div id="menu">
+          <div className="menu-btn btn-simple" data-skill={ESkillName.hacking}>
+            {SKILLS[ESkillName.hacking].name}
+          </div>
+          <div className="menu-btn btn-simple" data-skill={ESkillName.spy}>
+            {SKILLS[ESkillName.spy].name}
+          </div>
+          <div
+            className="menu-btn btn-simple"
+            data-skill={ESkillName.mercenary}>
+            {SKILLS[ESkillName.mercenary].name}
+          </div>
+          <div className="menu-btn btn-play">Старт/Отмена</div>
+        </div>
+        <canvas id="game" width="400" height="200"></canvas>
+      </div>
+    </>
+  )
+}
